8d23f5d9995c28348159ccb72cddbfb4b76cb230
nicolashiguera17 <189983438+nicolashiguera17@users.noreply.github.com>
2024-12-13 07:55:19 -0500

    solucion del ejercicio

---
+++ main.py
@@ -0,0 +1,5 @@
+cadena1=("Bienvenido al programa que genera contraseñas seguras.")
+cadena2=("el proposito es implementar un generador de contraseñas seguras que permite al usuario especificar la longitud de la contraseña y decidir si desea incluir mayúsculas, minúsculas, caracteres alfanuméricos y símbolos. La generación de contraseñas se realiza a través de una función que toma la longitud y las preferencias del usuario y devuelve una contraseña segura. El programa ofrece un menú interactivo para probar el generador de contraseñas y permite al usuario realizar múltiples solicitudes.")
+resultado= cadena1 + cadena2
+print (resultado)
+
